4f65bc923d5d3220042738472acf24fc30b50940
Vicente Vargas <1097813@est.intec.edu.do>
2023-03-15 18:53:16 -0400

    remove .next files

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Login from "./Login/login";
-import Dashboard from "./Dashboard/dashboard";
-import { useState } from "react";
-function VirtEdu() {
-  const [isLogged, setIsLogged] = useState(false);
-  return <>{isLogged ? <Dashboard /> : <Login setIsLogged={setIsLogged} />}</>;
-}
-
-export default VirtEdu;
