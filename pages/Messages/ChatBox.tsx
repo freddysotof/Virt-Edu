7d5f58b7ba225249763a7ed76c8962a23ae405a0
jose <josemirandaalmeyda@hotmail.com>
2023-03-20 19:47:52 -0400

    Messages Messages here

---
+++ pages/Messages/ChatBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './ChatBox.module.css';
+
+function ChatBox() {
+  return (
+    <div className={classes.chatBox}>
+      {/* aquí va el contenido de la caja de chat */}
+    </div>
+  );
+}
+
+export default ChatBox;
--- pages/Messages/Messages.tsx
+++ pages/Messages/Messages.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import { Layout } from '../../layout/Layout'
+export default Messages
+import React from 'react';
+import classes from './Messages.module.css';
+import NewChat from './NewChat';
+import SearchBar from './SearchBar';
+import ChatBox from './ChatBox';
+import SendMessage from './SendMessage';
+import { Layout } from '../../layout/Layout';
 
 function Messages() {
   return (
     <Layout>
         
+  
+    <div className={classes.messages}>
+      <div className={classes.contactsBox}>
+        <SearchBar />
+        <NewChat />
+      </div>
+      <div className={classes.chat}>
+        <ChatBox />
+        <SendMessage />
+      </div>
+    </div>
     </Layout>
-  )
+  );
 }
 
-export default Messages
+export default Messages;
