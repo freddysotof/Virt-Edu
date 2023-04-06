fd46f5a763076fbb311cd4f7b09f4388556e52f1
jose <josemirandaalmeyda@hotmail.com>
2023-04-05 23:56:09 -0400

    Added Assignment Counter

--- pages/Course/CourseCompletion/CourseCompletion.tsx
+++ pages/Course/CourseCompletion/CourseCompletion.tsx
@@ -3,20 +3,31 @@ import classes from './CourseCompletion.module.css';
 import Assignment from './Assignment';
 
 function CourseCompletion({ courseId }) {
-  // Supongamos que tiene un valor de progreso del 50%
+  const assignments = [
+    { id: "1", title: "suma bla bla bla bla bla bla bla bla bla bla", date: "1/2/2001", completed: true },
+    { id: "2", title: "resta", date: "2/2/2001", completed: true },
+    { id: "3", title: "multiplicacion", date: "3/2/2001", completed: false },
+    { id: "4", title: "Division ", date: "3/2/2001", completed: false },
+  ];
+
+  // Usamos reduce para contar la cantidad de asignaciones completadas
+  const completedAssignments = assignments.reduce((acc, curr) => curr.completed ? acc + 1 : acc, 0);
+  const totalAssignments = assignments.length;
   const progress = 10;
+
 
   return (
     <div className={classes.courseCompletion}>
       <h2 className={classes.title}>Course Completion</h2>
-      <div className={classes.progressText}>{`${progress}% completed`}</div>    
+      <div className = {classes.progressHead}>
+      <div className={classes.progressPercentage}>{`${progress}% completed`}</div> 
+      <div className={classes.progressText}>{`${completedAssignments} / ${totalAssignments}`}</div>
+      </div>   
       <div className={classes.progressBarContainer}>
-        <div className={classes.progressBarCompleted} style={{ width: `${progress}%`, backgroundColor: '#FFA800' }}></div>
+        <div className={classes.progressBarCompleted} style={{ width: `${(completedAssignments / totalAssignments) * 100}%`, backgroundColor: '#FFA800' }}></div>
       </div>
       <div className={classes.assignmentsContainer}>
-        <Assignment id="1" title="suma bla bla bla bla bla bla bla bla bla bla" date="1/2/2001" completed={false} />
-        <Assignment id="2" title="resta" date="2/2/2001" completed={true} />
-        <Assignment id="3" title="multiplicacion" date="3/2/2001" completed={false} />
+        {assignments.map((assignment) => <Assignment key={assignment.id} {...assignment} />)}
       </div>
     </div>
   );
