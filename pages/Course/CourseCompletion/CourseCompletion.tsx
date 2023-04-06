051d2e216752bef4291292a75c81e26e0ee17626
jose <josemirandaalmeyda@hotmail.com>
2023-04-06 00:15:05 -0400

    Added Assignment Numbers

--- pages/Course/CourseCompletion/CourseCompletion.tsx
+++ pages/Course/CourseCompletion/CourseCompletion.tsx
@@ -7,27 +7,31 @@ function CourseCompletion({ courseId }) {
     { id: "1", title: "suma bla bla bla bla bla bla bla bla bla bla", date: "1/2/2001", completed: true },
     { id: "2", title: "resta", date: "2/2/2001", completed: true },
     { id: "3", title: "multiplicacion", date: "3/2/2001", completed: false },
-    { id: "4", title: "Division ", date: "3/2/2001", completed: false },
+    { id: "6", title: "Division ", date: "3/2/2001", completed: true },
   ];
 
   // Usamos reduce para contar la cantidad de asignaciones completadas
   const completedAssignments = assignments.reduce((acc, curr) => curr.completed ? acc + 1 : acc, 0);
   const totalAssignments = assignments.length;
-  const progress = 10;
-
+  const progress = (completedAssignments / totalAssignments) * 100;
 
   return (
     <div className={classes.courseCompletion}>
       <h2 className={classes.title}>Course Completion</h2>
-      <div className = {classes.progressHead}>
-      <div className={classes.progressPercentage}>{`${progress}% completed`}</div> 
-      <div className={classes.progressText}>{`${completedAssignments} / ${totalAssignments}`}</div>
-      </div>   
+      <div className={classes.progressHead}>
+        <div className={classes.progressPercentage}>{`${progress}% completed`}</div>
+        <div className={classes.progressText}>{`${completedAssignments} / ${totalAssignments}`}</div>
+      </div>
       <div className={classes.progressBarContainer}>
         <div className={classes.progressBarCompleted} style={{ width: `${(completedAssignments / totalAssignments) * 100}%`, backgroundColor: '#FFA800' }}></div>
       </div>
       <div className={classes.assignmentsContainer}>
-        {assignments.map((assignment) => <Assignment key={assignment.id} {...assignment} />)}
+        {assignments.map((assignment, index) => (
+          <div key={assignment.id} className={classes.assignment}>
+            <span>{`${index + 1}.`}</span>
+            <Assignment {...assignment} />
+          </div>
+        ))}
       </div>
     </div>
   );
