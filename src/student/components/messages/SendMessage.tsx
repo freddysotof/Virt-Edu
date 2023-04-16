acef743dbeb65ae06330948b15fac9f6bd654669
Freddy Soto <fsoto@elcatador.com>
2023-04-16 18:00:06 -0400

    connect to signalR hub

--- src/student/components/messages/SendMessage.tsx
+++ src/student/components/messages/SendMessage.tsx
@@ -3,14 +3,14 @@ import classes from '../../assets/css/messages/SendMessage.module.css';
 import { MdSend } from 'react-icons/md';
 import { FaSmile } from 'react-icons/fa';
 
-function SendMessage() {
+function SendMessage({onClickSend}) {
   return (
     <div className={classes.sendMessage}>
       <button className={classes.emojiButton}>
         <FaSmile />
       </button>
       <input type="text" placeholder="Type a message..." />
-      <button className={classes.sendButton}>
+      <button className={classes.sendButton} onClick={onClickSend}>
         <MdSend />
       </button>
       
